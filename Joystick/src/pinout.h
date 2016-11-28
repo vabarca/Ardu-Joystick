4c7726eae681fb6e1e8ad518c7ad592e1d721e8e
vabarca <viabfer@gmail.com>
2016-11-28 20:47:04 +0100

    First commit

---
+++ Joystick/src/pinout.h
@@ -0,0 +1,79 @@
+//-----------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
+
+/** @file pinout.h
+ *  @date   November, 2015
+ *  @brief Teensy pinout assignment
+ *
+ *  This code is based on the LINX example firmware for use with
+ *  the PJRC Teensy 3.1 with the serial interface enabled.
+ *
+ *  For more information see:           www.labviewmakerhub.com/linx
+ *  For support visit the forums at:    www.labviewmakerhub.com/forums/linx
+ *
+ *  @author Cooked by Vicente A. (TT)
+ *  @bug No know bugs.
+ */
+
+//-----------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
+
+#ifndef PINOUT_H_
+#define PINOUT_H_
+
+//-----------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
+
+//---[ Pinout assignment: ]----------------------------------------------------
+
+/**
+ *
+ */
+const uint8_t YawTrimUp         (8);
+
+/**
+ *
+ */
+const uint8_t YawTrimDown         (8);
+
+
+/**
+ *
+ */
+const uint8_t PitchTrimUp         (8);
+
+/**
+ *
+ */
+const uint8_t PitchTrimDown         (8);
+
+
+/**
+ *
+ */
+const uint8_t RollTrimUp         (8);
+
+/**
+ *
+ */
+const uint8_t RollTrimDown         (8);
+
+/**
+ *
+ */
+const uint8_t ThrottleTrimUp         (8);
+
+
+/**
+ *
+ */
+const uint8_t ThrottleTrimDown         (8);
+
+
+//-----------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
+
+#endif /* PINOUT_H_ */
+
+//-----------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
---
+++ Joystick/src/types.h
@@ -0,0 +1,119 @@
+//-----------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
+
+/** @file  types.h
+ *  @date  November, 2015
+ *  @brief This file contains the definitions of special data types
+ *  		used in the application.
+ *
+ *
+ *  @author Cooked by Vicente A. (TT)
+ *  @bug No know bugs.
+ */
+
+//-----------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
+
+#ifndef _TYPES_H_
+ #define _TYPES_H_
+
+ //-----------------------------------------------------------------------------
+ //---[ Standard header files: ]------------------------------------------------
+
+ #if defined(ARDUINO) && ARDUINO >= 100
+   #include "Arduino.h"
+ #else
+   #include "WProgram.h"
+ #endif
+
+ //---[ Version defines: ]-------------------------------------------------------
+
+ /**
+  * Program Major version
+  */
+ const uint8_t  MAJOR     (1);
+ /**
+  * Program Minor version
+  */
+ const uint8_t  MINOR     (0);
+ /**
+  * Program bugs fixes
+  */
+ const uint8_t  PATCH     (0);
+
+//-----------------------------------------------------------------------------
+//---[ Macros: ]---------------------------------------------------------------
+
+#ifdef SERIAL_OUTPUT
+  #define  SERIAL_BEGIN         Serial.begin(115200)
+  #define  SERIAL_PRINTLN(a)    Serial.println(a)
+  #define  SERIAL_PRINT(a)      Serial.print(a)
+#else
+  #define  SERIAL_BEGIN
+  #define  SERIAL_PRINTLN(a)
+  #define  SERIAL_PRINT(a)
+#endif
+
+//---[ Generic data Types: ]---------------------------------------------------
+
+//---[ Application enumerated parameters: ]------------------------------------
+
+//---[ Application-specific data Types: ]-------------------------------------
+
+//---[ Global defines: ]-------------------------------------------------------
+
+/**
+ * EEPROM memory addresses for PITCH calibration data
+ */
+const uint8_t EEPROM_CAL_PITCH      (0);
+
+/**
+ * EEPROM memory addresses for ROLL calibration data
+ */
+const uint8_t EEPROM_CAL_ROLL       (EEPROM_CAL_PITCH + sizeof(float));
+
+/**
+ * EEPROM memory addresses for altitude calibration data
+ */
+const uint8_t EEPROM_CAL_ALTITUDE   (EEPROM_CAL_ROLL + sizeof(float));
+
+/**
+ * EEPROM memory addresses for altitude calibration data
+ */
+const uint8_t EEPROM_REF_ALTITUDE   (EEPROM_CAL_ALTITUDE + sizeof(float));
+
+/**
+ * EEPROM memory addresses for temp calibration data
+ */
+const uint8_t EEPROM_CAL_TEMP       (EEPROM_REF_ALTITUDE + sizeof(float));
+
+//---[ Useful Macros: ]--------------------------------------------------------
+
+/**
+ * Macro to clear a shared variable in an atomic operation (critical area)
+ */
+#ifndef CLEAR
+  #define CLEAR(x) { cli(); x = 0; sei(); }
+#endif
+
+/**
+ * Macro to clear individual bits in a SFR register
+ */
+#ifndef cbi
+  #define cbi(sfr, bit) (_SFR_BYTE(sfr) &= ~_BV(bit))
+#endif
+
+/**
+ * Macro to set individual bits in a SFR register
+ */
+#ifndef sbi
+  #define sbi(sfr, bit) (_SFR_BYTE(sfr) |= _BV(bit))
+#endif
+
+//-----------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
+
+#endif  // _TYPES_H_
+
+//-----------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
